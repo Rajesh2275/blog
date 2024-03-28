692e44a982caac21719515e6175e7525e9506f15
Rajesh2275 <rajeshdasari2275@gmail.com>
2024-03-28 13:26:35 +0530

    first commit

---
+++ cmail.py
@@ -0,0 +1,14 @@
+import smtplib
+from smtplib import SMTP 
+from email.message import EmailMessage
+def sendmail(to,subject,body):
+    server=smtplib.SMTP_SSL('smtp.gmail.com',465)
+    server.login('rajesh2275@gmail.com','muji btmj cjuv lmsu')
+    msg=EmailMessage()
+    msg['From']='rajesh2275@gmail.com'
+    msg['Subject']=subject
+    msg['To']=to
+    msg.set_content(body)
+    server.send_message(msg)
+    server.quit()
+    
